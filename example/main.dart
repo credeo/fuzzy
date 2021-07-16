a20905269e6474c8a63bcc187119977a588e2244
prtrifun <petar.trifunovic@credeo.de>
2021-07-16 10:40:20 +0200

    Add stations test

--- example/main.dart
+++ example/main.dart
@@ -1,46 +1,69 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:fuzzy/fuzzy.dart';
 
 void main() {
-  final bookList = [
-    'Old Man\'s War',
-    'The Lock Artist',
-    'HTML5',
-    'Right Ho Jeeves',
-    'The Code of the Wooster',
-    'Thank You Jeeves',
-    'The DaVinci Code',
-    'Angels & Demons',
-    'The Silmarillion',
-    'Syrup',
-    'The Lost Symbol',
-    'The Book of Lies',
-    'Lamb',
-    'Fool',
-    'Incompetence',
-    'Fat',
-    'Colony',
-    'Backwards, Red Dwarf',
-    'The Grand Design',
-    'The Book of Samson',
-    'The Preservationist',
-    'Fallen',
-    'Monster 1959',
-  ];
-  final fuse = Fuzzy(
-    bookList,
-    options: FuzzyOptions(
-      findAllMatches: true,
-      tokenize: true,
-      threshold: 0.5,
-    ),
-  );
+  init();
+}
 
-  final result = fuse.search('book');
+void init() async {
+  final String data = File('station_data.json').readAsStringSync();
 
-  print(
-      'A score of 0 indicates a perfect match, while a score of 1 indicates a complete mismatch.');
+  try {
+    Map<String, dynamic> jsonResult = json.decode(data) as Map<String, dynamic>;
+    List<Station> _stationsLocalData = [];
+    for (Map<String, dynamic>? s in jsonResult["stations"]["elements"]) {
+      if (s == null) continue;
+      final station = Station.fromJson(s);
+      if (station.id != null) {
+        _stationsLocalData.add(station);
+      }
+    }
+    final fuse = Fuzzy(
+      _stationsLocalData,
+      options: FuzzyOptions(
+        isCaseSensitive: false,
+        shouldSort: true,
+        findAllMatches: true,
+        minMatchCharLength: 1,
+        location: 0,
+        threshold: 0.5,
+        distance: 100,
+        keys: [
+          WeightedKey<Station>(
+            name: 'longName',
+            getter: (station) => station.longName,
+            weight: 1,
+          ),
+          WeightedKey<Station>(
+            name: 'place',
+            getter: (station) => station.place,
+            weight: 1,
+          ),
+        ],
+      ),
+    );
 
-  result.forEach((r) {
-    print('\nScore: ${r.score}\nTitle: ${r.item}');
-  });
+    final result = fuse.search('planeta');
+
+    print('A score of 0 indicates a perfect match, while a score of 1 indicates a complete mismatch.');
+
+    result.forEach((r) {
+      print('\nScore: ${r.score}\nstation: ${r.item.longName} ${r.item.place}');
+    });
+  } catch (e) {
+    print(e);
+  }
+}
+
+class Station {
+  final String? id;
+  final String longName;
+  final String place;
+
+  Station.fromJson(Map<String, dynamic> data)
+      : id = data['id'] as String,
+        longName = (data['longName'] as String?) ?? '',
+        place = (data['place'] as String?) ?? '';
 }
